46db6432057fbcc4e49619c935e85fea54c565a4
CTeillet <40306573+CTeillet@users.noreply.github.com>
2019-12-20 01:59:44 +0100

    Classe avec attribut

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -1,5 +1,6 @@
 package model;
 
 public class Joueur {
-
+	private String nom;
+	private int nbPionRestants;
 }
--- src/model/Plateau.java
+++ src/model/Plateau.java
@@ -1,5 +1,5 @@
 package model;
 
 public class Plateau {
-
+	private Pion[] plateau;
 }
